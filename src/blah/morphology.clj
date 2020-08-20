6e722ef24d84cbf49de2eceac70d942d2a9c8c7e
Stathis Sideris <stathis@pixelated-noise.com>
2020-08-20 19:30:17 +0300

    Morphology dispatch

--- src/blah/morphology.clj
+++ src/blah/morphology.clj
@@ -1,6 +1,7 @@
 (ns blah.morphology
   (:require [clojure.string :as str]
             [clojure.core.match :refer [match]]
+            [blah.lexicon :as lexicon]
             [blah.morphology.noun]
             [blah.morphology.verb]
             [blah.morphology.pronoun]
@@ -10,6 +11,25 @@
 (def verb blah.morphology.verb/verb)
 (def pronoun blah.morphology.pronoun/pronoun)
 (def adjective blah.morphology.adjective/adjective)
+
+(defn do-morphology [{:keys [base category non-morph lex-entry]
+                      :as   element}
+                     lexicon]
+  (if non-morph
+    base
+    (let [lex-entry (or lex-entry (lexicon/by-base base))]
+      (case category
+        :pronoun
+        (pronoun element)
+        :noun
+        (noun element lex-entry)
+        :verb
+        (verb element lex-entry)
+        :adjective
+        (adjective element lex-entry)
+        :adverb
+        "TODO"
+        base))))
 
 ;; (require '[blah.lexicon :as lex])
 ;; (def wolf (-> lex/lex (lex/by-base "wolf") first))
